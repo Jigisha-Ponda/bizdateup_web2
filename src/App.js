0729dc6884e4a2fc6e2d8b77fe5cbd0b48600bac
Jigisha <jigishaadatiya21@gmail.com>
2024-11-07 19:19:37 +0530

    Add the UI of some section of Resource page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./assets/css/App.css";
 import Home from './pages/Home';
+import StartAutoScroll from './pages/startAutoScroll';
+import Investor from './pages/Investors';
+import TabsLayout from './pages/TabsLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/scroll" element={<StartAutoScroll />} /> */}
+        <Route path="/tabs" element={<TabsLayout />} />
+
       </Routes>
     </BrowserRouter>
   );
